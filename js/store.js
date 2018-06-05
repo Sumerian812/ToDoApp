10e93f0b94adde3e2f10d6d9549c8f8bc2fe3cf5
Sumerian812 <veronica_hannibal@outlook.com>
2018-06-05 19:12:03 +0200

    removed now unnecessary todoId variable

--- js/store.js
+++ js/store.js
@@ -121,14 +121,13 @@
 	Store.prototype.remove = function (id, callback) {
 		var data = JSON.parse(localStorage[this._dbName]);
 		var todos = data.todos;
-		var todoId;
 		
 		for (var i = 0; i < todos.length; i++) {
 			if (todos[i].id === id) {
 				todos.splice(i, 1);
 			}
 		}
-		
+
 		localStorage[this._dbName] = JSON.stringify(data);
 		callback.call(this, todos);
 	};
